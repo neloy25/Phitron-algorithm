40583c9594e4f8c13ecc340e61222db1c7bbccf7
neloy25 <sadmansafinneloy678@gmail.com>
2025-08-22 15:16:42 +0600

    DFS on 2d grid bug fixed

--- Week1/Module3/dfs_on_2d_grid.cpp
+++ Week1/Module3/dfs_on_2d_grid.cpp
@@ -1,43 +1,41 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
 
-vector<vector<int>> grid;
-vector<vector<int>> visited;
+vector<vector<char>> grid;
+vector<vector<bool>> visited;
 vector<pair<int,int>> dxy = {{-1,0},{1,0},{0,-1},{0,1}};
-int n,m;
+int n, m;
 
-bool valid(int i,int j) {
-    if(i < 0 || i >= n || j < 0 || j >= m)
-        return false;
-    return true;
+bool valid(int i, int j) {
+    return (i >= 0 && i < n && j >= 0 && j < m);
 }
 
-void dfs(int si,int sj) {
-    cout << si << " " << sj << endl;
+void dfs(int si, int sj) {
+    cout << si << " " << sj << "\n";
     visited[si][sj] = true;
 
-    for(int i = 0; i < 4; i++) {
-        int ci = si + dxy[i].first;
-        int cj = sj + dxy[i].second;
-        if(valid(ci,cj) && !visited[ci][cj]) {
-            dfs(ci,cj);
+    for (auto [dx, dy] : dxy) {
+        int ci = si + dx;
+        int cj = sj + dy;
+        if (valid(ci, cj) && !visited[ci][cj]) {
+            dfs(ci, cj);
         }
     }
 }
 
-int main()
-{
+int main() {
     cin >> n >> m;
+    grid.resize(n, vector<char>(m));
+    visited.assign(n, vector<bool>(m, false));
 
-    grid.resize(n,vector<int>(m));
-    visited.assign(n,vector<int>(m,false));
-    for(int i = 0; i < n; i++)
-        for(int j = 0; j < n; j++)
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < m; j++) {
             cin >> grid[i][j];
+        }
+    }
 
-    int si,sj;
+    int si, sj;
     cin >> si >> sj;
-    dfs(si,sj);
-    return 0;
-}
 
+    dfs(si, sj);
+}
